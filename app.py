452c01ea59939bf6fdd0e2c6f8c35a0ad8e4289b
Siddhant Giri <25.siddhant.giri3@gmail.com>
2025-04-24 09:46:36 +0530

    app.py

--- app.py
+++ app.py
@@ -2,51 +2,53 @@ import streamlit as st
 import pickle
 import numpy as np
 
-# import the model
 # Load the trained model and dataset
 pipe = pickle.load(open('pipe.pkl','rb'))
 df = pickle.load(open('df.pkl','rb'))
 
-
 st.title("Laptop Predictor")
 
-# brand
-company = st.selectbox('Brand',df['Company'].unique())
+# Brand
+company = st.selectbox('Brand', df['Company'].unique())
 
-# type of laptop
-type = st.selectbox('Type',df['TypeName'].unique())
+# Type of laptop
+type = st.selectbox('Type', df['TypeName'].unique())
 
-# Ram
-ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
+# RAM
+ram = st.selectbox('RAM(in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
 
-# weight
+# Weight
 weight = st.number_input('Weight of the Laptop')
 
 # Touchscreen
-touchscreen = st.selectbox('Touchscreen',['No','Yes'])
+touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
 
 # IPS
-ips = st.selectbox('IPS',['No','Yes'])
+ips = st.selectbox('IPS', ['No', 'Yes'])
 
-# screen size
-screen_size = st.slider('Scrensize in inches', 10.0, 18.0, 13.0)
+# Screen size
+screen_size = st.slider('Screen size in inches', 10.0, 18.0, 13.0)
 
-# resolution
-resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
+# Resolution
+resolution = st.selectbox('Screen Resolution', ['1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800', '2560x1600', '2560x1440', '2304x1440'])
 
-#cpu
-cpu = st.selectbox('CPU',df['Cpu brand'].unique())
+# CPU
+cpu = st.selectbox('CPU', df['Cpu brand'].unique())
 
-hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+# HDD
+hdd = st.selectbox('HDD(in GB)', [0, 128, 256, 512, 1024, 2048])
 
-ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+# SSD
+ssd = st.selectbox('SSD(in GB)', [0, 8, 128, 256, 512, 1024])
 
-gpu = st.selectbox('GPU',df['Gpu brand'].unique())
+# GPU
+gpu = st.selectbox('GPU', df['Gpu brand'].unique())
 
-os = st.selectbox('OS',df['os'].unique())
+# OS
+os = st.selectbox('OS', df['os'].unique())
 
 if st.button('Predict Price'):
-    # query
+    # Query for prediction
     ppi = None
     if touchscreen == 'Yes':
         touchscreen = 1
@@ -60,8 +62,18 @@ if st.button('Predict Price'):
 
     X_res = int(resolution.split('x')[0])
     Y_res = int(resolution.split('x')[1])
-    ppi = ((X_res**2) + (Y_res**2))**0.5/screen_size
-    query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
+    ppi = ((X_res**2) + (Y_res**2))**0.5 / screen_size
 
-    query = query.reshape(1,12)
-    st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
+    query = np.array([company, type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
+
+    # Reshape query for prediction
+    query = query.reshape(1, 12)
+
+    # Ensure the pipeline handles the encoding and scaling
+    prediction = pipe.predict(query)
+
+    # Apply the inverse transformation (if needed)
+    predicted_price = int(np.exp(prediction[0]))  # Only if the model was trained with a log-transformed target
+
+    st.title(f"The predicted price of this configuration is ₹{predicted_price}")
+
